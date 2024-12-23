5735cbe99a676a43ea246820973adb14ed1f1da1
KAPIL KHEMSHETTE <98579837+KapilKhemshette@users.noreply.github.com>
2024-12-23 10:40:43 +0530

    Add files via upload

---
+++ Java/Activities/Activity7.java
@@ -0,0 +1,10 @@
+package Module_1.Com.FST_2;
+
+public class Activity7 {
+    public static void main(String args[]) {
+        MountainBike mb = new MountainBike(3, 0, 25);
+        System.out.println(mb.bicycleDesc());
+        mb.speedUp(20);
+        mb.applyBrake(5);
+    }
+}
